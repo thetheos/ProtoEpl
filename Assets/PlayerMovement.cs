37d68850b83bb0371239a66ab6e05409aadae65f
Theos <vandendriesschetheo@hotmail.com>
2019-01-22 15:33:27 +0100

    First Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float xMvt = 0f;
+    float yMvt = 0f;
+
+    public float runspeed = 40f;
+
+    public CharacterController2D controller2D;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMvt = Input.GetAxis("Horizontal") * runspeed;
+        yMvt = Input.GetAxis("Vertical") * runspeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller2D.Move(xMvt * Time.fixedDeltaTime, yMvt * Time.fixedDeltaTime);
+    }
+}
